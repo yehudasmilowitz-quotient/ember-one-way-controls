271e8333015780472c2908314d3de10edfe16a97
Yehuda Smilowitz <ysmilowitz@quotient.com>
2024-03-21 15:36:02 -0400

    undo

--- index.js
+++ index.js
@@ -3,9 +3,4 @@
 
 module.exports = {
   name: 'ember-one-way-controls',
-
-  included(app) {
-    this.addonDependencies = this.addonDependencies || [];
-    this.addonDependencies.push({ name: 'ember-cli-htmlbars' });
-  }
 };
