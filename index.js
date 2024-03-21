a6016d3be39da756d3156cfd99ee647a02be8e9e
Yehuda Smilowitz <ysmilowitz@quotient.com>
2024-03-21 15:04:20 -0400

    try another idea to fix ember-cli-htmlbars issue

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-one-way-controls'
+  name: 'ember-one-way-controls',
+
+  included(app) {
+    this.addonDependencies = this.addonDependencies || [];
+    this.addonDependencies.push({ name: 'ember-cli-htmlbars' });
+  }
 };
